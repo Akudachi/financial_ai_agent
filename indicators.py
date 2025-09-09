c2b9f25404b2c0549709b03988bf9b906c787307
Adarsh kudachi <105845369+Akudachi@users.noreply.github.com>
2025-09-09 12:30:38 +0530

    indicators.py

--- indicators.py
+++ indicators.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import ta
 
 def add_indicators(df):
@@ -6,7 +6,7 @@ def add_indicators(df):
     df['RSI'] = ta.momentum.rsi(df['Close'], window=14)
     df['MACD'] = ta.trend.macd_diff(df['Close'])
     return df
-=======
+
 import ta
 
 def add_indicators(df):
@@ -14,4 +14,4 @@ def add_indicators(df):
     df['RSI'] = ta.momentum.rsi(df['Close'], window=14)
     df['MACD'] = ta.trend.macd_diff(df['Close'])
     return df
->>>>>>> 647a1a0ecdbb67439beb8685a67f4906de223262
+
