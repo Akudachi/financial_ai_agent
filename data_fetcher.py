e3b9969d798fcd4e5244d7d927cd18b0959426e8
Adarsh kudachi <105845369+Akudachi@users.noreply.github.com>
2025-09-09 12:30:14 +0530

    data_fetcher.py

--- data_fetcher.py
+++ data_fetcher.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import yfinance as yf
 import pandas as pd
 
@@ -6,7 +6,7 @@ def get_stock_data(ticker="AAPL", period="6mo", interval="1d"):
     data = yf.download(ticker, period=period, interval=interval)
     data.dropna(inplace=True)
     return data
-=======
+
 import yfinance as yf
 import pandas as pd
 
@@ -14,4 +14,4 @@ def get_stock_data(ticker="AAPL", period="6mo", interval="1d"):
     data = yf.download(ticker, period=period, interval=interval)
     data.dropna(inplace=True)
     return data
->>>>>>> 647a1a0ecdbb67439beb8685a67f4906de223262
+
