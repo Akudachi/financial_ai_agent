52fbbe7a43ae2846fb48e8238482228d88870d5d
Adarsh kudachi <105845369+Akudachi@users.noreply.github.com>
2025-09-09 12:31:08 +0530

    sentiment.py

--- sentiment.py
+++ sentiment.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 from transformers import pipeline
 
 sentiment_model = pipeline("sentiment-analysis")
@@ -6,7 +6,7 @@ sentiment_model = pipeline("sentiment-analysis")
 def analyze_sentiment(text):
     result = sentiment_model(text)[0]
     return result['label'], result['score']
-=======
+
 from transformers import pipeline
 
 sentiment_model = pipeline("sentiment-analysis")
@@ -14,4 +14,4 @@ sentiment_model = pipeline("sentiment-analysis")
 def analyze_sentiment(text):
     result = sentiment_model(text)[0]
     return result['label'], result['score']
->>>>>>> 647a1a0ecdbb67439beb8685a67f4906de223262
+
